43f2008f3654998a53d53b85f961bd65fc82db14
Nicola Zilio <nicolazilio@gmail.com>
2026-09-18 14:38:26 +0200

    DJANGO_SETTINGS_MODULE is config

--- common/utils/run_tenant_tasks.py
+++ common/utils/run_tenant_tasks.py
@@ -10,7 +10,7 @@ from pathlib import Path
 project_root = Path(__file__).resolve().parent.parent.parent
 sys.path.append(str(project_root))
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "benchbaze.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 import django
 
